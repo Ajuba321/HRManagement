db2e16c612e272ca357904880bf1dde8fcaabf58
Narendra_bannu <narendra.katta@cognine.com>
2021-02-23 19:10:43 +0530

    Added Documentsupload modelclass

---
+++ Models/DocumentsUpload.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRManagement.Models
+{
+    public class DocumentsUpload
+    {
+        public int DocumentId { get; set; }
+        public string FileType { get; set; }
+        public string FileName { get; set; }
+        public FormFile File { get; set; }
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+    }
+}
